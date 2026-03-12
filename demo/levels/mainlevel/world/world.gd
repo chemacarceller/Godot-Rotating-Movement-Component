2cc1041cbeb81d64733f7ef79fbb53ba44980406
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-03-12 07:22:03 +0100

    Delete demo directory

--- demo/levels/mainlevel/world/world.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	$"RigidBodies/Cube 1".apply_impulse(Vector3(3,9,2)) 
-	$"RigidBodies/Cube 2".apply_impulse(Vector3(6,12,3)) 
-	$"RigidBodies/Cube 3".apply_impulse(Vector3(9,15,4)) 
