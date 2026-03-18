b360669ffff2d1918aeed3907d6ba878e2d2b143
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-03-18 15:48:11 +0100

    Add files via upload

--- rotating_movement.cpp
+++ rotating_movement.cpp
@@ -18,18 +18,25 @@ void RotatingMovementComponent::_bind_methods() {
 
     // Exporting properties (equivalent to @export)
     ADD_PROPERTY(PropertyInfo(Variant::BOOL, "_isEnabled"), "set_enabled", "is_enabled");
-    ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "rotatingVector"), "set_rotating_vector", "get_rotating_vector");
-    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "speed"), "set_speed", "get_speed");
+    ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "_rotatingVector"), "set_rotating_vector", "get_rotating_vector");
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "_speed"), "set_speed", "get_speed");
 }
 
-RotatingMovementComponent::RotatingMovementComponent() {}
+// Constructor and Destructor
+RotatingMovementComponent::RotatingMovementComponent() {
+    speedRad = _speed * Math_PI / 30.0f;
+    rotatingVectorNormalized = _rotatingVector.normalized();
+}
+
 RotatingMovementComponent::~RotatingMovementComponent() {}
 
+// Called when the node enters the scene tree for the first time.
 void RotatingMovementComponent::_ready() {
     // Equivalent to @onready var _parentActor = get_parent()
-    _parentActor = Object::cast_to<Node3D>(get_parent());
+    parentActor = Object::cast_to<Node3D>(get_parent());
 }
 
+// Getters and Setters
 void RotatingMovementComponent::set_enabled(bool p_enabled) {
     _isEnabled = p_enabled;
 }
@@ -39,35 +46,38 @@ bool RotatingMovementComponent::is_enabled() const {
 }
 
 void RotatingMovementComponent::set_rotating_vector(Vector3 p_vector) {
-    rotatingVector = p_vector;
-    if (rotatingVector != Vector3(0, 0, 0)) {
-        _rotatingVectorNormalized = rotatingVector.normalized();
+    _rotatingVector = p_vector;
+    if (_rotatingVector != Vector3(0, 0, 0)) {
+        rotatingVectorNormalized = _rotatingVector.normalized();
     }
 }
 
 Vector3 RotatingMovementComponent::get_rotating_vector() const {
-    return rotatingVector;
+    return _rotatingVector;
 }
 
 void RotatingMovementComponent::set_speed(float p_speed) {
-    speed = p_speed;
+    _speed = p_speed;
+
     // RPM to Radians/second conversion: (RPM * 2 * PI) / 60
-    _speedRad = p_speed * Math_PI / 30.0f;
+    speedRad = p_speed * Math_PI / 30.0f;
 }
 
 float RotatingMovementComponent::get_speed() const {
-    return speed;
+    return _speed;
 }
 
+// Called every physics frame. 'delta' is the elapsed time since the previous frame.
 void RotatingMovementComponent::_notification(int p_what) {
     if (p_what == NOTIFICATION_WM_CLOSE_REQUEST) {
         queue_free();
     }
 }
 
+// Called every physics frame. 'delta' is the elapsed time since the previous frame.
 void RotatingMovementComponent::_physics_process(double delta) {
-    if (_isEnabled && _parentActor != nullptr) {
+    if (_isEnabled && parentActor != nullptr) {
         // In C++ we use _speedRad to avoid recalculating the constant in each frame
-        _parentActor->rotate(_rotatingVectorNormalized, _speedRad * (float)delta);
+        parentActor->rotate(rotatingVectorNormalized, speedRad * (float)delta);
     }
 }
--- rotating_movement.h
+++ rotating_movement.h
@@ -8,23 +8,32 @@
 namespace godot {
 
     class RotatingMovementComponent : public Node {
+        // Macro to register the class with Godot's type system.
         GDCLASS(RotatingMovementComponent, Node)
 
         private:
+
+            // Underscored private variable for exported variables (following Godot's convention)
+            // Speed in RPM (Rotations Per Minute)
+            float _speed = 0.0f;
+            Vector3 _rotatingVector = Vector3(0, 1, 0);
             bool _isEnabled = true;
-            Vector3 rotatingVector = Vector3(0, 0, 0);
-            Vector3 _rotatingVectorNormalized = Vector3(0, 0, 0);
-            float speed = 0.0f;
-            float _speedRad = 0.0f;
-            Node3D* _parentActor = nullptr;
+            
+            // Private variables for internal use without exposing to Godot not underscored 
+            Vector3 rotatingVectorNormalized = Vector3(0, 0, 0);
+            float speedRad = 0.0f;
+            Node3D* parentActor = nullptr;
 
         protected:
+            // Method to bind properties and methods to Godot   
             static void _bind_methods();
 
         public:
+            // Constructor and Destructor
             RotatingMovementComponent();
             ~RotatingMovementComponent();
 
+            // Overridden methods from Node
             void _ready() override;
             void _physics_process(double delta) override;
             void _notification(int p_what);
